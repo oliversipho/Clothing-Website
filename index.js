22cc96b492a6fbc7c4aab4f6f2f5e82e1d13aa22
oliversipho <103258773+oliversipho@users.noreply.github.com>
2023-04-01 15:04:23 +0200

    Update...

--- index.js
+++ index.js
@@ -6,3 +6,31 @@ function myFunction() {
       x.className = "topnav";
     }
   }
+
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      450: {
+        slidesPerView: 2,
+        spaceBetween: 0,
+      },
+      820: {
+        slidesPerView: 1,
+        spaceBetween: 0,
+      },
+      1024: {
+        slidesPerView: 2,
+        spaceBetween:0,
+      },
+    },
+  });
+
+
+  $('.s-checkbox').on('change', function() {
+    $('.s-checkbox').not(this).prop('checked', false);  
+});
